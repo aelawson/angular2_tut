c3e10eb2fd0bddcff6127c13a47f4e8bd0df5e00
Andrew Lawson <aeriklawson@gmail.com>
2016-04-16 00:55:03 -0400

    Finished. Adding Map Component.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,51 @@
 import {Component} from 'angular2/core';
 import {HeroService} from './hero.service';
 import {HeroesComponent} from './heroes.component';
+import {HeroDetailComponent} from './hero-detail.component';
+import {DashboardComponent} from './dashboard.component';
+import {MapComponent} from './map.component';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 
+@RouteConfig([
+    {
+        path: '/heroes',
+        name: 'Heroes',
+        component: HeroesComponent
+    },
+    {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: DashboardComponent,
+        useAsDefault: true
+    },
+    {
+        path: '/detail/:id',
+        name: 'HeroDetail',
+        component: HeroDetailComponent
+    },
+    {
+        path: '/map',
+        name: 'Map',
+        component: MapComponent
+    }
+])
 @Component({
-    selector: "my-app",
+    selector: 'my-app',
     template: `
         <h1>{{title}}</h1>
-        <my-heroes></my-heroes>
+        <nav>
+            <a [routerLink]="['Dashboard']">Dashboard</a>
+            <a [routerLink]="['Heroes']">Heroes</a>
+            <a [routerLink]="['Map']">Map</a>
+        </nav>
+        <router-outlet></router-outlet>
     `,
-    directives: [HeroesComponent],
+    directives: [
+        ROUTER_DIRECTIVES
+    ],
     providers: [
-        HeroService
+        HeroService,
+        ROUTER_PROVIDERS
     ]
 })
 export class AppComponent {
