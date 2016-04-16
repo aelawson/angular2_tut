62a949003b7e98f3c8ba297c4b1b46dfb8194bcb
Andrew Lawson <aeriklawson@gmail.com>
2016-04-15 23:45:45 -0400

    More progress.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,46 +1,18 @@
 import {Component} from 'angular2/core';
-
-export class Hero {
-    id: number;
-    name: string;
-}
+import {HeroService} from './hero.service';
+import {HeroesComponent} from './heroes.component';
 
 @Component({
-    selector: 'my-app',
+    selector: "my-app",
     template: `
-    <h1>{{title}}</h1>
-    <h2>My Heroes</h2>
-    <ul class="heroes">
-        <li *ngFor="#hero of heroes">
-            <span class="badge">{{hero.id}}</span> {{hero.name}}
-        </li>
-    </ul>
-    <h2>{{hero.name}}</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
-    </div>
-    `
+        <h1>{{title}}</h1>
+        <my-heroes></my-heroes>
+    `,
+    directives: [HeroesComponent],
+    providers: [
+        HeroService
+    ]
 })
 export class AppComponent {
-    public heroes = Heroes;
-    title = 'Tour of Heroes';
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    }
+    title = "Tour of Heroes";
 }
-
-var Heroes: Hero[] = [
-    { "id": 11, "name": "Mr. Nice" },
-    { "id": 12, "name": "Narco" },
-    { "id": 13, "name": "Bombasto" },
-    { "id": 14, "name": "Celeritas" },
-    { "id": 15, "name": "Magneta" },
-    { "id": 16, "name": "RubberMan" },
-    { "id": 17, "name": "Dynama" },
-    { "id": 18, "name": "Dr IQ" },
-    { "id": 19, "name": "Magma" },
-    { "id": 20, "name": "Tornado" }
-];
